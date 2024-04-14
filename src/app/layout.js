48267a595f7cee4d1d6d1de326d0fce8f18b7733
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-04-14 13:21:23 -0300

    @

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,22 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-const inter = Inter({ subsets: ['latin'] })
-
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+const inter = Inter({ subsets: ["latin"] });
+import { Analytics } from "@vercel/analytics/react";
 export const metadata = {
-  title: 'Dublin Store',
-  description: 'Descubre lo último en tecnología de punta con nuestra tienda de importación. Encuentra gadgets exclusivos, electrónica avanzada y dispositivos inteligentes. Servicio personalizado y envío rápido. ¡Explora nuestras ofertas hoy!',
-}
+  title: "Dublin Store",
+  description:
+    "Descubre lo último en tecnología de punta con nuestra tienda de importación. Encuentra gadgets exclusivos, electrónica avanzada y dispositivos inteligentes. Servicio personalizado y envío rápido. ¡Explora nuestras ofertas hoy!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
